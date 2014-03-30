4d4f7c1f55822e5f4849f289b126005916a5ff9f
Joesus <joesusnick@gmail.com>
2014-03-29 19:13:56 -0600

    adding remote

--- exercises/all_pattern_test.rb
+++ exercises/all_pattern_test.rb
@@ -17,7 +17,7 @@ class AllPatternTest < Minitest::Test
     numbers = [0, 0, 0, 0, 1, 0, 0, 0]
     all_zeros = true
     numbers.each do |number|
-      # Your code goes here
+      all_zeros = false if number != 0
     end
     refute all_zeros
   end
