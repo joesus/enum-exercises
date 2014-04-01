6e1201fe51936234b41f29f1aeee86d581f2f1dd
Joesus <joesusnick@gmail.com>
2014-03-31 19:15:01 -0600

    Completed some more tests.

--- test/find_pattern_test.rb
+++ test/find_pattern_test.rb
@@ -5,7 +5,6 @@ require 'minitest/pride'
 class FindPatternTest < Minitest::Test
 
   def test_find_first_seven_letter_word
-    skip
     words = ["capricious", "berry", "unicorn", "bag", "apple", "festering", "pretzel", "pencil"]
     found = nil
     words.each do |word|
@@ -18,69 +17,66 @@ class FindPatternTest < Minitest::Test
   end
 
   def test_no_waldo
-    skip
     words = ["scarf", "sandcastle", "flag", "pretzel", "crow", "key"]
     found = nil
     words.each do |word|
-      # Your code goes here
+      if word.include?("waldo")
+        found = word
+      end
     end
     assert_equal nil, found
   end
 
   def test_find_waldo
-    skip
     words = ["noise", "dog", "fair", "house", "waldo", "bucket", "fish"]
     found = nil
-    # Your code goes here
+    words.each do |word|
+      if word.include?("waldo")
+        found = word
+      end
+    end
     assert_equal "waldo", found
   end
 
   def test_cannot_find_3_letter_words
-    skip
     words = ["piglet", "porridge", "bear", "blueberry"]
-    # Your code goes here
+    found = nil unless words.any? { |x| x.length < 4 }
     assert_equal nil, found
   end
 
   def test_find_13
-    skip
     numbers = [2, 13, 19, 8, 3, 27]
-    # Your code goes here
+    found = numbers.find { |x| x == 13 }
     assert_equal 13, found
   end
 
   def test_find_first_even_number
-    skip
     numbers = [3, 7, 13, 11, 10, 2, 17]
-    # Your code goes here
+    found = numbers.find { |x| x % 2 == 0 }
     assert_equal 10, found
   end
 
   def test_find_first_multiple_of_3
-    skip
     numbers = [2, 8, 9, 27, 24, 5]
-    # Your code goes here
+    found = numbers.find { |x| x % 3 == 0 }
     assert_equal 9, found
   end
 
   def test_find_first_word_starting_with_q
-    skip
     words = ["weirdo", "quill", "fast", "quaint", "quitter", "koala"]
-    # Your code goes here
+    found = words.find { |word| word.start_with?("q") }
     assert_equal "quill", found
   end
 
   def test_find_first_word_ending_with_er
-    skip
     words = ["biggest", "pour", "blight", "finger", "pie", "border"]
-    # Your code goes here
+    found = words.find { |word| word.end_with?("er") }
     assert_equal "finger", found
   end
 
   def test_find_first_number_greater_than_20
-    skip
     numbers = [1, 8, 19, 21, 29, 31, 34]
-    # Your code goes here
+    found = numbers.find { |num| num > 20 }
     assert_equal 21, found
   end
 
